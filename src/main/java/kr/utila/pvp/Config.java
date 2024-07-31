759f936cdbc64575f745ca2bb241ae46756c0e6c
shininghyunho <gusgh3315@uos.ac.kr>
2024-07-31 14:31:40 +0900

    Config 상수 주석 추가

--- src/main/java/kr/utila/pvp/Config.java
+++ src/main/java/kr/utila/pvp/Config.java
@@ -1,24 +1,30 @@
 package kr.utila.pvp;
 
 public class Config {
-
     private static final Main PLUGIN = Main.getInstance();
-
-    // 경기 초기화 시간
+    /**
+     * 경기 리셋 시간
+     */
     public static int MATCH_RESET_SECOND;
-    // 경기 대기 시간
+    /**
+     * 경기 시작 대기 시간
+     */
     public static int MATCH_WAIT_SECOND;
-    // 경기 시간
+    /**
+     * 경기 시간
+     */
     public static int MATCH_SECOND;
-    // 탈주 유저 대기 시간
+    /**
+     * 탈주한 유저 대기 시간
+     */
     public static int WAITING_FOR_LEFT_USER_SECOND;
 
     public static void load() {
         PLUGIN.saveDefaultConfig();
         PLUGIN.reloadConfig();
-        MATCH_WAIT_SECOND = PLUGIN.getConfig().getInt("MATCH_WAIT_SECOND");
-        WAITING_FOR_LEFT_USER_SECOND = PLUGIN.getConfig().getInt("WAITING_FOR_LEFT_USER_SECOND");
-        MATCH_SECOND = PLUGIN.getConfig().getInt("MATCH_SECOND");
         MATCH_RESET_SECOND = PLUGIN.getConfig().getInt("MATCH_RESET_SECOND");
+        MATCH_WAIT_SECOND = PLUGIN.getConfig().getInt("MATCH_WAIT_SECOND");
+        MATCH_SECOND = PLUGIN.getConfig().getInt("MATCH_SECOND");
+        WAITING_FOR_LEFT_USER_SECOND = PLUGIN.getConfig().getInt("WAITING_FOR_LEFT_USER_SECOND");
     }
 }
